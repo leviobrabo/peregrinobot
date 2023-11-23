60e088b5c7527f675294e23ef3870f2374d5c4e2
leviobrabo <carlosjunior20313@gmail.com>
2023-11-22 21:19:31 -0300

    add msgstart

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -2360,6 +2360,54 @@ bot.on("callback_query", async (query) => {
       },
     });
   } else if (data === "back_to_start") {
+    const msgstart = `Olá, <b>${firstName}</b>! \n\nEu sou o bot <b>Peregrino</b>, sou um bot bíblico que está aqui para propagar o evangelho de Deus, e ajudá-los nos estudos diários da bíblia.\n\nAdicione-me em seu grupo para receber as mensagens bíblicas.\n\n<b>Funções:</b> /help <b>[COMECE POR AQUI]</b>\n\n📦<b>Meu código-fonte:</b> <a href="https://github.com/leviobrabo/Peregrino">GitHub</a>`;
+    const options_start = {
+      parse_mode: "HTML",
+      disable_web_page_preview: true,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "📖 Bíblia",
+              switch_inline_query_current_chat: '',
+            },
+          ],
+          [
+            {
+              text: "🙏 Pedidos de oração",
+              url: "https://t.me/pedidosdeoracaoperegrino",
+            },
+            {
+              text: "🪪 Minha conta",
+              callback_data: "minha_conta",
+            }
+          ],
+          [
+            {
+              text: "✨ Adicione-me em seu grupo",
+              url: "https://t.me/operegrino_bot?startgroup=true",
+            },
+          ],
+          [
+            {
+              text: "⚙️ Atualizações do bot",
+              url: "https://t.me/peregrinochannel",
+            },
+            {
+              text: "💡 Sobre",
+              callback_data: "edit_caption",
+            },
+          ],
+          [
+            {
+              text: "📍 Canal Oficial",
+              url: "https://t.me/peregrinosbr",
+            },
+          ],
+        ],
+      },
+    };
+
     await bot.editMessageText(msgstart, {
       parse_mode: "HTML",
       chat_id: chatId,

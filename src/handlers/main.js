23b6cccf03d7b74c870d8991052c34b0f9fcd775
leviobrabo <carlosjunior20313@gmail.com>
2023-11-22 21:14:33 -0300

    add first name

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -1966,6 +1966,7 @@ bot.on("callback_query", async (query) => {
       },
     })
   } else if (data === "back_to_help") {
+    const firstName = query.message.from.first_name;
     const text =
       `<b>Olá! ${firstName}</b>\n\nEu sou um bot com o objetivo de propagar o evangelho pelo Telegram. Estou aqui para ajudá-lo em sua jornada espiritual e proporcionar uma experiência enriquecedora com recursos e comandos relacionados à Palavra de Deus. Fique à vontade para explorar as opções abaixo e descobrir tudo o que posso oferecer:\n\n<b>Funções disponíveis:</b>\n\n- Plano de leitura bíblica em 365 dias (18h)\n- Planos diversos(21h30min)\n- Adicione seus motivos de oração e crie lembretes\n- Adicione anotações bíblicas e reveja\n- Tenha um monitoramento de dias de estudo bíblico\n- Envio de versículos diários e versículos com tema para grupos\n- Pesquisa da Bíblia inline\n- Escolha a sua tradução preferida\n- Pedidos de intercessões`;
     const options = {
